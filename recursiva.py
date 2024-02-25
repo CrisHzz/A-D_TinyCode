418f085d023e6a9e7f93da5839c2b7868a31bce6
Kalichhe <jhondh2003@gmail.com>
2024-02-24 20:45:29 -0500

    Adding asymptotic notation

--- recursiva.py
+++ recursiva.py
@@ -1,61 +1,51 @@
 # Calcule la suma de los números impares por un lado y los números pares por otro entre dos números enteros dados (suponga que el primero es menor que el segundo, e incluya los números extremos en la suma)
 
 
-def sumaParesImpares(number1, number2):
-    # Aqui estamos validando que si number1 es mayor que number2, pues para que no se petatee la ram por el Stack Overflow
-    if number1 > number2:
+def sumaParesImpares(number1, number2):  # O(n)
+    if number1 > number2:  # O(0)
         # Esto es un caso base, porque aqui validamos si number1 es igual a number2
-        if number1 == number2:
-            # Aqui cuando son iguales, validamos si number1 es par o impar
-            if number1 % 2 == 0:
+        if number1 == number2:  # O(0)
+            if number1 % 2 == 0:  # O(0)
                 # Aqui retornamos una tupla con el valor de number1 y 0
-                return number1, 0
-            # Aqui lo mismo que en el if de arriba, validamos si number2 es par o impar
-            else:
+                return number1, 0  # O(0)
+            else:  # O(0)
                 # Aqui lo mismo, retornamos una tupla con el valor de 0 y number2
-                return 0, number1
-        else:
-            # Aqui validamos que number1 sea par y si no lo es se ira por el else
-            if number1 % 2 == 0:
+                return 0, number1  # O(0)
+        else:  # O(0)
+            if number1 % 2 == 0:  # O(0)
                 # Aqui hacemos el llamado recursivo de la funcion, pero le restamos 1 a number1 para que en cada iteracion esta se vaya acercando cada vez mas a number2 y le pasamos tambien el number2
-                pares, impares = sumaParesImpares(number1 - 1, number2)
-                # Aqui retornamos una tupla con la suma de number1 y pares, y el valor de impares
-                return number1 + pares, impares
-            else:
+                pares, impares = sumaParesImpares(number1 - 1, number2)  # O(n)
+                return number1 + pares, impares  # O(0)
+            else:  # O(0)
                 # Aqui hacemos el llamado recursivo de la funcion, pero le restamos 1 a number1 para que en cada iteracion esta se vaya acercando cada vez mas a number2 y le pasamos tambien el number2
-                pares, impares = sumaParesImpares(number1 - 1, number2)
-                # Aqui es lo mismo pero en vez de sumar number1 con pares, lo hacemos con impares
-                return pares, number1 + impares
-    # Se hira por el aqui si number1 no es mayor que number2
-    else:
+                pares, impares = sumaParesImpares(number1 - 1, number2)  # O(n)
+                return pares, number1 + impares  # O(0)
+    else:  # O(0)
         # Esto es un caso base, porque aqui validamos si number1 es igual a number2
-        if number1 == number2:
-            # Aqui cuando son iguales, validamos si number1 es par o impar
-            if number1 % 2 == 0:
+        if number1 == number2:  # O(0)
+            if number1 % 2 == 0:  # O(0)
                 # Aqui retornamos una tupla con el valor de number1 y 0
-                return number1, 0
-            # Aqui lo mismo que en el if de arriba, validamos si number2 es par o impar
-            else:
+                return number1, 0  # O(0)
+            else:  # O(0)
                 # Aqui lo mismo, retornamos una tupla con el valor de 0 y number2
-                return 0, number1
-        else:
-            # Aqui validamos que number1 sea par y si no lo es se ira por el else
-            if number1 % 2 == 0:
+                return 0, number1  # O(0)
+        else:  # O(0)
+            if number1 % 2 == 0:  # O(0)
                 # Aqui hacemos el llamado recursivo de la funcion, pero le sumamos 1 a number1 para que en cada iteracion esta se vaya acercando cada vez mas a number2 y le pasamos tambien el number2
-                pares, impares = sumaParesImpares(number1 + 1, number2)
-                # Aqui retornamos una tupla con la suma de number1 y pares, y el valor de impares
-                return number1 + pares, impares
-            else:
+                pares, impares = sumaParesImpares(number1 + 1, number2)  # O(n)
+                return number1 + pares, impares  # O(0)
+            else:  # O(0)
                 # Aqui hacemos el llamado recursivo de la funcion, pero le sumamos 1 a number1 para que en cada iteracion esta se vaya acercando cada vez mas a number2 y le pasamos tambien el number2
-                pares, impares = sumaParesImpares(number1 + 1, number2)
-                # Aqui es lo mismo pero en vez de sumar number1 con pares, lo hacemos con impares
-                return pares, number1 + impares
+                pares, impares = sumaParesImpares(number1 + 1, number2)  # O(n)
+                return pares, number1 + impares  # O(0)
 
 
-number1 = int(input("Ingresa un número: "))
-number2 = int(input("Ingresa un número: "))
+number1 = int(input("Ingresa un número: "))  # O(0)
+number2 = int(input("Ingresa un número: "))  # O(0)
 
-pares, impares = sumaParesImpares(number1, number2)
+pares, impares = sumaParesImpares(number1, number2)  # O(n)
 
-print("La suma de los números pares es: ", pares)
-print("La suma de los números impares es: ", impares)
+print("La suma de los números pares es: ", pares)  # O(0)
+print("La suma de los números impares es: ", impares)  # O(0)
+
+# Entonces la complejidad total del algoritmo es: 26 O(n) + 5 O(n) = O(n)
