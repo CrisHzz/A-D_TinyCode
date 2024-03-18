4072a71290b80bd0599e5912bf00d0cd5185ce39
Kalichhe <jhondh2003@gmail.com>
2024-03-17 23:09:43 -0500

    Melo

--- Taller2/src/myModels/combinations.py
+++ Taller2/src/myModels/combinations.py
@@ -3,13 +3,13 @@ from operation import Operation
 
 class Combination(Operation): # O(0)
     def __init__(self): # O(0)
-        self.n = int(input("Ingrese la cantidad de elementos (n): ")) # O(0)
-        self.r = int(input("Ingrese la cantidad de grupos (r): ")) # O(0)
+        self.n = int(input("\033[96;1m" + "Ingrese la cantidad de elementos (n): " + "\033[0m")) # O(0)
+        self.r = int(input("\033[96;1m" + "Ingrese la cantidad de grupos (r): " + "\033[0m")) # O(0)
     # Entonces la complejidad de esta parte es: 2 O(0) = O(0)
 
     def ordinary_operation(self): # O(0)
         if self.n < self.r: # O(0)
-            raise ValueError("El valor de n debe ser mayor o igual que r") # O(0)
+            raise ValueError("\033[91;1m" + "El valor de n debe ser mayor o igual que r" + "\033[0m") # O(0)
 
         numerator = self.classicFactorial(self.n) # O(0)
         denominator = self.classicFactorial(self.r) * self.classicFactorial(self.n - self.r) # O(0)
--- Taller2/src/myModels/menu.py
+++ Taller2/src/myModels/menu.py
@@ -1,5 +1,10 @@
 import sys
+
 sys.path.append("/home/kalichhe/Desktop/A-D_TinyCode/Taller2/src/myModels")
+
+from colorama import init
+
+init()
 
 from permutation import Permutation
 from combinations import Combination
@@ -7,72 +12,85 @@ from varations import Variations
 from principlesCounting import PrinciplesCounting
 
 
-class Menu():  # O(n)
+class Menu:  # O(n)
     def __init__(self):  # O(n)
         while True:  # O(n)
             self.decision = int(
                 input(
-                    "\nSelecciona \n(1) Técnicas de conteo \n(2) Técnicas de combinatorias \n(3) Para salir \nopción: "
+                    "\033[93;1m"
+                    + "\nSelecciona \n(1) Técnicas de conteo \n(2) Técnicas de combinatorias \n(3) Para salir \nopción: "
+                    + "\033[0m"
                 )
             )  # O(n)
             if self.decision == 3:  # O(n)
                 break  # O(n)
             elif self.decision == 2:  # O(n)
                 self.decision = int(
-                    input("\n\t¿Importa el orden? \n\t(1) Si \n\t(2) No \n\tOpción: ")
+                    input(
+                        "\033[94;1m"
+                        + "\n\t¿Importa el orden? \n\t(1) Si \n\t(2) No \n\tOpción: "
+                        + "\033[0m"
+                    )
                 )  # O(n)
                 if self.decision == 1:  # O(n)
                     self.decision = int(
                         input(
-                            "\n\t\t¿Intervienen todos los elementos? \n\t\t(1) Si \n\t\t(2) No \n\t\tOpción: "
+                            "\033[95;1m"
+                            + "\n\t\t¿Intervienen todos los elementos? \n\t\t(1) Si \n\t\t(2) No \n\t\tOpción: "
+                            + "\033[0m"
                         )
                     )  # O(n)
                     if self.decision == 2:  # O(n)
                         self.decision = int(
-                            input(
-                                "\n\t\t\t¿Se repiten los elementos? \n\t\t\t(1) Si \n\t\t\t(2) No \n\t\t\topción: "
+                            input("\033[91;1m" +
+                                "\n\t\t\t¿Se repiten los elementos? \n\t\t\t(1) Si \n\t\t\t(2) No \n\t\t\topción: " + "\033[0m"
                             )
                         )  # O(n)
                         if self.decision == 2:  # O(n)
-                            print("\nVariations ordinary")  # O(n)
+                            print("\033[97;1m" + "\nVariations ordinary" + "\033[0m")  # O(n)
                             operation = Variations()  # O(n)
-                            print(operation.ordinary_operation())  # O(n)
+                            print("\033[92;1m" + operation.ordinary_operation() + "\033[0m")  # O(n)
                         else:  # O(n)
-                            print("\nVariations with repetitions")  # O(n)
+                            print("\033[97;1m" + "\nVariations with repetitions" + "\033[0m")  # O(n)
                             operation = Variations()  # O(n)
-                            print(operation.variation_operation())  # O(n)
+                            print("\033[92;1m" + operation.variation_operation() + "\033[0m")  # O(n)
                     else:  # O(n)
                         self.decision = int(
-                            input(
-                                "\n\t\t\t¿¿Se repiten los elementos? \n\t\t\t(1) Si \n\t\t\t(2) No \n\t\t\topción: "
+                            input("\033[91;1m" + 
+                                "\n\t\t\t¿¿Se repiten los elementos? \n\t\t\t(1) Si \n\t\t\t(2) No \n\t\t\topción: " + "\033[0m"
                             )
                         )  # O(n)
                         if self.decision == 2:  # O(n)
-                            print("\nPermutations ordinary")  # O(n)
+                            print("\033[97;1m" + "\nPermutations ordinary" + "\033[0m")  # O(n)
                             operation = Permutation()  # O(n)
-                            print(operation.ordinary_operation())  # O(n)
+                            print("\033[92;1m" + operation.ordinary_operation() + "\033[0m")  # O(n)
                         else:  # O(n)
-                            print("\nPermutations with repetitions")  # O(n)
+                            print("\033[97;1m" + "\nPermutations with repetitions" + "\033[0m")  # O(n)
                             operation = Permutation()  # O(n)
-                            print(operation.variation_operation())  # O(n)
+                            print("\033[92;1m" + operation.variation_operation() + "\033[0m")  # O(n)
                 else:  # O(n)
                     self.decision = int(
-                        input(
-                            "\n\t\t¿Se repiten los elementos? \n\t\t(1) Si \n\t\t(2) No \n\t\topción: "
+                        input("\033[91;1m" + 
+                            "\n\t\t¿Se repiten los elementos? \n\t\t(1) Si \n\t\t(2) No \n\t\topción: " + "\033[0m"
                         )
                     )  # O(n)
                     if self.decision == 2:  # O(n)
-                        print("\nCombinations ordinary")  # O(n)
+                        print("\033[97;1m" + "\nCombinations ordinary" + "\033[0m")  # O(n)
                         operation = Combination()  # O(n)
-                        print(operation.ordinary_operation())  # O(n)
+                        print("\033[92;1m" + operation.ordinary_operation() + "\033[0m")  # O(n)
                     else:  # O(n)
-                        print("\nCombinations with repetitions")  # O(n)
+                        print("\033[97;1m" + "\nCombinations with repetitions" + "\033[0m")  # O(n)
                         operation = Combination()  # O(n)
-                        print(operation.variation_operation())  # O(n)
-            else:  # O(n)
+                        print("\033[92;1m" + operation.variation_operation() + "\033[0m")  # O(n)
+            elif self.decision == 1:  # O(n)
                 operation = PrinciplesCounting()  # O(n)
-                print(operation.list_multiplier())  # O(n)
+                print("\033[97;1m" + "\nPrinciples of counting" + "\033[0m") # O(n)
+                print("\033[92;1m" + operation.list_multiplier() + "\033[0m")  # O(n)
+                
+            else: # O(n)
+                print("\033[91;1m" + "Opción no válida" + "\033[0m") # O(n)
 
-    # Entonces la complejidad de esta parte es: 41 O(n) = O(n)
+    # Entonces la complejidad de esta parte es: 43 O(n) = O(n)
+
 
 # Entonces la complejidad total del algoritmo es: 1 O(0) + 1 O(n) = O(n)
