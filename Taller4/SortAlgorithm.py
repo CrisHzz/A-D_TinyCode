fb09c28af19664de38075336bdbdcf1c9c970151
Anamariaalucema <140823861+Anamariaalucema@users.noreply.github.com>
2024-05-03 19:39:39 -0500

    Update SortAlgorithm.py

--- Taller4/SortAlgorithm.py
+++ Taller4/SortAlgorithm.py
@@ -59,29 +59,41 @@ class SortAlgorithm:
         except (TypeError, ValueError) as e:
             print("Ingrese una lista válida de números")  # Imprimimos el mensaje de error
     
-    def counting_sort(self,array:list, descending:bool=False):
-        max_val = max(array)
-        count = [0] * (max_val + 1)
-        
-        for num in array:
-            count[num] += 1
-        
-        i = 0
-        if descending:
-            for num in range(max_val, -1, -1):
-                while count[num] > 0:
-                    array[i] = num
-                    i += 1
-                    count[num] -= 1
-        else:
-            for num in range(len(count)):
-                while count[num] > 0:
-                    array[i] = num
-                    i += 1
-                    count[num] -= 1
-        
-        return array
+    def counting_sort(array:list, descending:bool=False)->list:
+        try:
+            if not isinstance(array, list):
+                raise TypeError("El parámetro 'array' debe ser una lista.")
 
+            if not isinstance(descending, bool):
+                raise TypeError("El parámetro 'descending' debe ser un valor booleano.")
+
+            if not all(isinstance(element, (int)) for element in array):
+                raise ValueError("Los elementos del 'array' deben ser números enteros")
+
+            max_val = max(array)
+            count = [0] * (max_val + 1)
+
+            for num in array:
+                count[num] += 1
+
+            i = 0
+            if descending:
+                for num in range(max_val, -1, -1):
+                    while count[num] > 0:
+                        array[i] = num
+                        i += 1
+                        count[num] -= 1
+            else:
+                for num in range(len(count)):
+                    while count[num] > 0:
+                        array[i] = num
+                        i += 1
+                        count[num] -= 1
+
+            return array
+
+        except (TypeError, ValueError) as e:
+            print("Ingrese una lista válida de números enteros")  # Imprimimos el mensaje de error
 
 
 prueba=SortAlgorithm()
